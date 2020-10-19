5b2ab9655ea8068aad43e7d364427b3c141fa4cc
anton97johansson <anton97jo@gmail.com>
2020-10-19 17:20:51 +0100

    saves chat to nosql database

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const express = require('express');
+// const { MongoClient } = require("mongodb");
 const app = express();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+// const uri = 'mongodb://localhost:27017';
+// const client = new MongoClient(uri);
+const mongo = require("mongodb").MongoClient;
+const dsn = "mongodb://localhost:27017/chat";
 io.origins(['https://antonscript.me:443']);
 
-io.on('connection', function (socket) {
+io.on('connection', async function (socket) {
     var time = new Date().toLocaleString();
     console.info("User connected");
     io.emit('chat message', `[${time}] User connected`);
+    // await insertToCol(`[${time}] User connected`).catch(console.dir);
 
-    socket.on('chat message', function (message) {
+    socket.on('chat message', async function (message) {
         message["time"] = new Date().toLocaleString();
         io.emit('chat message', `[${message["time"]}] ${message["nick"]}: ${message["msg"]}`);
         console.info(message);
+        await insertToCol(`[${message["time"]}] ${message["nick"]}: ${message["msg"]}`).catch(console.dir);
     });
 });
 
 server.listen(8300);
+
+async function insertToCol(msg) {
+    const client  = await mongo.connect(dsn);
+    const db = await client.db();
+    const col = await db.collection("messages");
+    const result = await col.insertOne({ message: msg });
+    console.log(
+      `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
+    );
+    await client.close();
+}
+
